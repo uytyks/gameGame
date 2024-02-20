b4ebff56d5233d1c9bd4be2128dbf7fbbe6a8e65
uytyks <uytyks@udel.edu>
2024-02-20 14:32:55 -0500

    FIXED OVERFLOW PROBLEM RAAA

--- chatBox.js
+++ chatBox.js
@@ -5,10 +5,21 @@ document.getElementById("pInput").addEventListener("keypress", function(event) {
     }});
 
 function addText(){
-    var ul = document.getElementsByClassName("chatBox");
-    var li = document.createElement("li");
+    var start = document.getElementsByClassName("lastText");
+    var newText = document.createElement("p");
+    newText.className = "lastText";
     var text = document.getElementById("pInput");
-    li.appendChild(document.createTextNode(text.value));
-    ul[0].appendChild(li);
+    newText.appendChild(document.createTextNode(text.value));
+    start[start.length-1].insertAdjacentElement("afterend",newText);
     text.value = "";
+    if(isOverflown(document.getElementById("mainBox"))){
+      while(isOverflown(document.getElementById("mainBox"))){
+        var start = document.getElementsByClassName("lastText");
+        start[0].remove();
+      }
+    }
+}
+
+function isOverflown(element) {
+  return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
 }
